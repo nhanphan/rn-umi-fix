edc5d29aaa5d7b3b3575e884d06e84fff4f047e0
Nhan Phan <nhan.phan@gmail.com>
2024-04-17 12:56:46 -0700

    example compiling expo app

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createV1 } from '@metaplex-foundation/mpl-core'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { generateSigner } from '@metaplex-foundation/umi';
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Create V1" onPress={async () => {
+        const umi = createUmi('https://api.devnet.solana.com')
+        const asset = generateSigner(umi)
+
+        await createV1(umi, {
+          asset,
+          name: 'test',
+          uri: 'test.com'
+        }).sendAndConfirm(umi)
+
+      }} />
     </View>
   );
 }
